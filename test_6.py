a650b0079138f9bd2fe4a4a6f342a985400e857c
A-l-e-s-t <74547061+A-l-e-s-t@users.noreply.github.com>
2023-05-11 09:25:45 +0300

    added gitignore

--- test_6.py
+++
@@ -1,4 +0,0 @@
-"""
-Write a function that takes string as an argument and returns a list of all possible combinations of the string.
-For example, if the string is 'abc', the function should return ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'].
-"""
